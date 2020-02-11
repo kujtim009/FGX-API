ca5c1846a3a4d3aa1d63f6c03fcd7f0b1c2893f9
kujtim009 <kujtim009@gmail.com>
2020-02-11 13:13:32 +0100

    adapted for deployment

--- src/menu-items.js
+++ src/menu-items.js
@@ -122,48 +122,48 @@ export default {
       ]
     },
     {
-      id: "pages",
-      title: "Pages",
+      id: "docu",
+      title: "DOCUMENTATION",
       type: "group",
       icon: "icon-pages",
       children: [
-        {
-          id: "auth",
-          title: "Authentication",
-          type: "collapse",
-          icon: "feather icon-lock",
-          badge: {
-            title: "New",
-            type: "label-danger"
-          },
-          children: [
-            {
-              id: "signup-1",
-              title: "Sign up",
-              type: "item",
-              url: "/auth/signup-1",
-              target: true,
-              breadcrumbs: false
-            },
-            {
-              id: "signin-1",
-              title: "Sign in",
-              type: "item",
-              url: "/auth/signin-1",
-              target: true,
-              breadcrumbs: false
-            }
-          ]
-        },
+        // {
+        //   id: "auth",
+        //   title: "Authentication",
+        //   type: "collapse",
+        //   icon: "feather icon-lock",
+        //   badge: {
+        //     title: "New",
+        //     type: "label-danger"
+        //   },
+        //   children: [
+        //     {
+        //       id: "signup-1",
+        //       title: "Sign up",
+        //       type: "item",
+        //       url: "/auth/signup-1",
+        //       target: true,
+        //       breadcrumbs: false
+        //     },
+        //     {
+        //       id: "signin-1",
+        //       title: "Sign in",
+        //       type: "item",
+        //       url: "/auth/signin-1",
+        //       target: true,
+        //       breadcrumbs: false
+        //     }
+        //   ]
+        // },
 
-        {
-          id: "sample-page",
-          title: "Sample Page",
-          type: "item",
-          url: "/sample-page",
-          classes: "nav-item",
-          icon: "feather icon-sidebar"
-        },
+        // {
+        //   id: "sample-page",
+        //   title: "Sample Page",
+        //   type: "item",
+        //   url: "/sample-page",
+        //   classes: "nav-item",
+        //   icon: "feather icon-sidebar"
+        // },
         {
           id: "docs",
           title: "Documentation",
@@ -171,15 +171,15 @@ export default {
           url: "/docs",
           classes: "nav-item",
           icon: "feather icon-help-circle"
-        },
-        {
-          id: "disabled-menu",
-          title: "Disabled Menu",
-          type: "item",
-          url: "#",
-          classes: "nav-item disabled",
-          icon: "feather icon-power"
         }
+        // {
+        //   id: "disabled-menu",
+        //   title: "Disabled Menu",
+        //   type: "item",
+        //   url: "#",
+        //   classes: "nav-item disabled",
+        //   icon: "feather icon-power"
+        // }
         /*{
                     id: 'buy-now',
                     title: 'Buy Now',
